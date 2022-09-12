dc91174c75fef6ae0e940928ec82daa16266f21a
karmokarsurjit <karmokarsurjit@gmail.com>
2022-09-12 18:28:06 +0600

    Register Page

--- lib/modules/register/Register/register_mobile.dart
+++ lib/modules/register/Register/register_mobile.dart
@@ -8,173 +8,177 @@ class RegisterPageMobilePortrait extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xff32302C),
-        body: ListView(
-          children: [
-            const SizedBox(height: 200,),
-            Image.asset('assets/images/LoginIcon.png', height: MediaQuery.of(context).size.height * .15,),
-            const SizedBox(height: 50,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.00),
-              child: TextField(
-                decoration: InputDecoration(
-                  focusedBorder :OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
+        backgroundColor: const Color(0xff32302C),
+        body: Center(
+          child: ListView(
+            children: [
+              const SizedBox(height: 20,),
+              Image.asset('assets/images/RegIcon.png', height: MediaQuery.of(context).size.height * .15,),
+              const SizedBox(height: 30,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.00),
+                child: TextField(
+                  decoration: InputDecoration(
+                    focusedBorder :OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    labelText: "Your name",
+                    labelStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                    ),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
-                  ),
-                  labelText: "Your name",
-                  labelStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15.0,
-                  ),
+                  keyboardType: TextInputType.name,
                 ),
-                keyboardType: TextInputType.name,
               ),
-            ),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.00),
-              child: TextField(
-                decoration: InputDecoration(
-                  focusedBorder :OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.00),
+                child: TextField(
+                  decoration: InputDecoration(
+                    focusedBorder :OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    labelText: "Email",
+                    labelStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                    ),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
-                  ),
-                  labelText: "Email",
-                  labelStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15.0,
-                  ),
+                  keyboardType: TextInputType.emailAddress,
                 ),
-                keyboardType: TextInputType.emailAddress,
               ),
-            ),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.00),
-              child: TextField(
-                decoration: InputDecoration(
-                  focusedBorder :OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.00),
+                child: TextField(
+                  decoration: InputDecoration(
+                    focusedBorder :OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    labelText: "Phone number",
+                    labelStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                    ),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
-                  ),
-                  labelText: "Phone number",
-                  labelStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15.0,
-                  ),
+                  keyboardType: TextInputType.phone,
                 ),
-                keyboardType: TextInputType.phone,
               ),
-            ),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.00),
-              child: TextField(
-                decoration: InputDecoration(
-                  focusedBorder :OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.00),
+                child: TextField(
+                  decoration: InputDecoration(
+                    focusedBorder :OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    labelText: "Password",
+                    labelStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                    ),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
-                  ),
-                  labelText: "Password",
-                  labelStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15.0,
-                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
                 ),
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
               ),
-            ),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.00),
-              child: TextField(
-                decoration: InputDecoration(
-                  focusedBorder :OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.00),
+                child: TextField(
+                  decoration: InputDecoration(
+                    focusedBorder :OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xffF2C94C), width: 2,),
+                    ),
+                    labelText: "Confirm Password",
+                    labelStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 15.0,
+                    ),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Color(0xffF2C94C), width: 2,),
-                  ),
-                  labelText: "Password",
-                  labelStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 15.0,
-                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
                 ),
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
               ),
-            ),
 
-        const Padding(padding:EdgeInsets.symmetric(vertical: 0.0,horizontal: 100.00),
-          child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Color(0xffF2C94C)
+              const SizedBox(height: 10,),
+          const Padding(padding:EdgeInsets.symmetric(vertical: 5.0,horizontal: 30.00),
+            child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Color(0xffF2C94C)
+                  ),
                 ),
-              ),
-              onPressed: null,
-              child:
-              Text('Log in',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black,fontSize: 15.0),)
+                onPressed: null,
+                child:
+                Text('Register',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black,fontSize: 15.0),)
+            ),
           ),
-        ),
-            Row(children: [
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text("Already have an account?",
-                  textAlign:  TextAlign.center,
-                  style:
-                  TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
+              Row(children: const [
+                SizedBox(width: 100),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text("Already have an account?",
+                    //textAlign:  TextAlign.center,
+                    style:
+                    TextStyle(
+                      fontSize: 15,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text("Login Now",
-                  textAlign:  TextAlign.center,
-                  style:
-                  TextStyle(
-                    fontSize: 15,
-                    color: Color(0xffF2C94C),
+                Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text("Login Now",
+                    //textAlign:  TextAlign.center,
+                    style:
+                    TextStyle(
+                      fontSize: 15,
+                      color: Color(0xffF2C94C),
+                    ),
                   ),
                 ),
-              ),
+              ],
+                ),
             ],
-              ),
-          ],
+          ),
         )
     );
   }
